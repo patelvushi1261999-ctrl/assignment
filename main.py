659d294fa6cb751905f65109960d4b94bc7162d7
vrushi patel <patelvushi1261999@gmail.com>
2026-07-02 13:08:08 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+#assignmnet of python
+
+tuple = (1,"hello",True,[1,2,3])
+
+print("The truple with different datra types:",tuple)
+
+number_tuple = (10,20,30,40)
+print("frgsdgfdg : ".number_tuple[2])
