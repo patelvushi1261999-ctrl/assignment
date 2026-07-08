0c77a3d59a665436afdff577d797ab8e957d44ba
vrushi patel <patelvushi1261999@gmail.com>
2026-07-08 16:44:14 +0530

    Added insta_intro.ipyng and hello_app.py

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ print("The truple with different datra types:",tuple)
 
 number_tuple = (10,20,30,40)
 print("frgsdgfdg : ".number_tuple[2])
+
+
+ls = [1,2,3,4,5]
+
+print("The list is:",ls)
+
+
+a=12
+print("The value of a is:",a)
